b1428e7aebc83c86913177ba54c91a2969f23a09
Ekene N <ekenayy@gmail.com>
2020-10-07 16:15:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,8 +7,9 @@ def find_even_values(src)
 while row_index < spice_rack.count do
   element_index = 0
   while element_index < spice_rack[row_index].count do
-   if spice_rack[row_index][element_index].even? 
+    if spice_rack[row_index][element_index].even? 
     p spice_rack[row_index][element_index]
+     end
     element_index += 1
   end
   row_index += 1
